225d411d89b942b538522d85efb0083ef53fdb4f
Devansh Rajput <devanshrajput032006@gmail.com>
2025-07-26 19:31:11 +0530

    eslint desable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,12 @@
 // eslint.config.mjs
-
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 import next from 'eslint-config-next';
 
 export default [
-  ...next,
+  ...next(),
   {
-    plugins: {
-      react: eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
-    },
     rules: {
-      // Disable unescaped entities warning
+      // Disable rules if needed
       'react/no-unescaped-entities': 'off',
-      // Allow JSX syntax parsing
-      'react/jsx-uses-react': 'off', // Not needed in React 17+
       'react/react-in-jsx-scope': 'off',
     },
   },
