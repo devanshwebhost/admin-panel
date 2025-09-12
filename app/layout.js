396638e471f1ba98c57833cf093baf53747dbe36
Devansh Rajput <devanshrajput032006@gmail.com>
2025-09-12 19:07:56 +0530

    adding google verifiation code

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* SEO meta tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Indocs Media" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        
+        {/* Google site verification */}
+        <meta
+          name="google-site-verification"
+          content="bmKNUPDQQDedvtwaI9MwDsLVBwHQFcS9Q6gFmy26lTY"
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} scroll-smooth  antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} scroll-smooth antialiased`}
       >
         <Providers>
-          <ToastContainer position="top-right" className={'z-[999]'} autoClose={3000} />
+          <ToastContainer position="top-right" className={"z-[999]"} autoClose={3000} />
           {children}
         </Providers>
       </body>
